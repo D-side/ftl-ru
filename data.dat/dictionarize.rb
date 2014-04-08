911274f63f9a3c6b340ad324aec065ee3961af9d
D-side <side.spb@gmail.com>
2014-04-09 03:22:24 +0400

    Fixes: 4 files left suspicious

--- data.dat/dictionarize.rb
+++ data.dat/dictionarize.rb
@@ -3,7 +3,7 @@ total = 0
 suspicious = []
 
 checklist = Dir.entries("./data.old.en")
-#checklist = ["events_fuel.xml"]
+#checklist = ["nameEvents.xml"]
 
 checklist.each do |source_file|
   #source_file = "tooltips.xml"
@@ -12,40 +12,44 @@ checklist.each do |source_file|
   if [".", ".."].include?(source_file)
     next
   end
-  puts "Now processing file: " + source_file
 
   ru = File.read("./xml.ru.utf8/" + source_file)
 
-  tooltips_ru = ru.scan(/>([\(\)\«А-ЯA-Z][\(\)0-9,ёA-ZА-Яа-я[[:space:]]\.\:\-\/\«\»\"\!\?\—\;\']+)\<\//).collect {|i| i[0] }
+  tooltips_ru = ru.scan(/>([\%\_0-9\(\)\«А-ЯA-Z][\%\_\(\)0-9,ёA-Za-zА-Яа-я[[:space:]]\.\:\-\/\«\»\"\!\?\—\;\']+)\<\//).collect {|i| i[0] }
 
   # Sweep original file
   en = File.read("./data.old.en/" + source_file)
 
-  tooltips_en = en.scan(/>([\(\)\"\ A-Z][\(\)0-9A-Za-z\:[[:space:]]\,\'\.\-\/\"\!\?\;]+)\<\//).collect {|i| i[0] }
+  tooltips_en = en.scan(/>([\%\_0-9\(\)\"\ A-Z][\%\_\(\)0-9A-Za-z\:[[:space:]]\,\'\.\-\/\"\!\?\;]+)\<\//).collect {|i| i[0] }
 
   en_ae = File.read("./data/" + source_file)
 
-  puts "\tRU: " + tooltips_ru.length.to_s + "\tEN: " + tooltips_en.length.to_s
   if tooltips_ru.length != tooltips_en.length
+    puts "Now processing file: " + source_file
+    puts "Entries:\tRU: " + tooltips_ru.length.to_s + "\tEN: " + tooltips_en.length.to_s
     puts "[!] WARNING: unequal phrase banks' lengths, investigate it"
+    puts ""
     suspicious << (source_file)
   end
 
   tooltips = {}
   0.upto(tooltips_ru.length - 1) { |i|
     tooltips[tooltips_en[i]] = tooltips_ru[i]
-    #puts "ru: " + tooltips_ru[i]
-    #puts "en: " + tooltips_en[i]
-    #puts "Replacing..."
+    if (checklist.length < 3)
+      puts "#{i+1}/#{tooltips_ru.length - 1}"
+      puts "ru: " + tooltips_ru[i]
+      puts "en: " + tooltips_en[i]
+      puts "Replacing..."
+      $stdin.readchar
+    end
     en_ae.gsub!(tooltips_en[i], tooltips_ru[i])
     # Used to be a pause
-    #$stdin.readchar
     }
 
   result = File.open("result/" + source_file, "wt")
   result.write(en_ae)
   result.close
-  puts ""
+
   total += 1
 end
 puts "Total files processed: #{total}, #{suspicious.length} of them are suspicious and should be looked at:"
