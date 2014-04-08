153c7bf07ffc0da84822ec3ef91a617bb34ef9ab
D-side <side.spb@gmail.com>
2014-04-09 02:34:57 +0400

    Lots'a fixes

--- data.dat/dictionarize.rb
+++ data.dat/dictionarize.rb
@@ -1,5 +1,11 @@
 # Sweep translated file
-Dir.entries("./data.old.en").each do |source_file|
+total = 0
+suspicious = []
+
+checklist = Dir.entries("./data.old.en")
+#checklist = ["events_fuel.xml"]
+
+checklist.each do |source_file|
   #source_file = "tooltips.xml"
   #Nonono, not this one anymore, we're going generic
 
@@ -10,23 +16,26 @@ Dir.entries("./data.old.en").each do |source_file|
 
   ru = File.read("./xml.ru.utf8/" + source_file)
 
-  tooltips_ru = ru.scan(/>([\«А-ЯA-Z][0-9,ёA-ZА-Яа-я[[:space:]]\.\:\-\/\«\»\"\!\?\—\;\']+)\<\//).collect {|i| i[0] }
+  tooltips_ru = ru.scan(/>([\(\)\«А-ЯA-Z][\(\)0-9,ёA-ZА-Яа-я[[:space:]]\.\:\-\/\«\»\"\!\?\—\;\']+)\<\//).collect {|i| i[0] }
 
   # Sweep original file
   en = File.read("./data.old.en/" + source_file)
 
-  tooltips_en = en.scan(/>([\"\ A-Z][0-9A-Za-z\:[[:space:]]\,\'\.\-\/\"\!\?\;]+)\<\//).collect {|i| i[0] }
+  tooltips_en = en.scan(/>([\(\)\"\ A-Z][\(\)0-9A-Za-z\:[[:space:]]\,\'\.\-\/\"\!\?\;]+)\<\//).collect {|i| i[0] }
 
   en_ae = File.read("./data/" + source_file)
 
   puts "\tRU: " + tooltips_ru.length.to_s + "\tEN: " + tooltips_en.length.to_s
-  puts "[!] WARNING: unequal phrase banks' lengths, investigate it" unless tooltips_ru.length == tooltips_en.length
+  if tooltips_ru.length != tooltips_en.length
+    puts "[!] WARNING: unequal phrase banks' lengths, investigate it"
+    suspicious << (source_file)
+  end
+
   tooltips = {}
   0.upto(tooltips_ru.length - 1) { |i|
     tooltips[tooltips_en[i]] = tooltips_ru[i]
     #puts "ru: " + tooltips_ru[i]
     #puts "en: " + tooltips_en[i]
-
     #puts "Replacing..."
     en_ae.gsub!(tooltips_en[i], tooltips_ru[i])
     # Used to be a pause
@@ -37,4 +46,7 @@ Dir.entries("./data.old.en").each do |source_file|
   result.write(en_ae)
   result.close
   puts ""
+  total += 1
 end
+puts "Total files processed: #{total}, #{suspicious.length} of them are suspicious and should be looked at:"
+puts suspicious.join(" ")
