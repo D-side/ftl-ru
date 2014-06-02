2970573f45e546a3bd60db9dc77e21c41d2c4369
D-side <side.spb@gmail.com>
2014-06-03 03:52:24 +0400

    Update

--- data.dat/compose_notabenoid.rb
+++ data.dat/compose_notabenoid.rb
@@ -1,13 +1,18 @@
 #path_list = ["data/tooltips.xml"]
-list_file = File.open("events.flst")
-path_list = list_file.collect { |line| File.join("result", line.chomp) }
-
+list_file = File.open("rem.flst")
+path_list = []
+path_list << list_file.collect { |line|
+	[File.join("result", line.chomp), File.join("data", line.chomp)]
+}
+path_list << File.join("data", "misc.xml")
 #path_list =	Dir.entries("data").collect do |e|
 #	f = File.join("data", e)
 #	f if !File.directory?(f)
 #end
-path_list.compact! # That's an order!
-
+puts path_list.inspect
+path_list.compact!
+puts path_list.inspect
+path_list.flatten! # That's an order!
 blacklist = %w(autoReward fleet clone)
 # Общий чёрный список тегов, не нуждающихся в переводе
 #blacklist = %w(img ship x y rarity bp cost startPower maxPower level type locked sp fireChance breachChance cooldown power image damage shots sound weaponArt speed ion droneSlots weaponSlots number direction shieldImage cloakImage boardingAI value tip iconImage weaponBlueprint droneImage dodge missiles persDamage sysDamage explosion stunChance r g b length target stackable multiDifficulty autoReward name hullBust radius spin drone_targetable projectile amount count chargeLevels stun)
@@ -20,7 +25,7 @@ path_list.each do |path|
 	output_path = "lists/#{path}.list";
 	input = File.open(path, "rt");
 	output = File.open(output_path, "wt")
-	phrase_list =
+
 	phrase_list = input.collect do |line|
 		matches = /<([A-Za-z_]+).*>([^<]+)<\/\1>/.match(line)
 		if matches and
